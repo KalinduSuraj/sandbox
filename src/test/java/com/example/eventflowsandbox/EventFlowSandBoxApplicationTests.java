bd86335483a63381d39a3e183b9f153b3bbc971e
KalinduSuraj <GADSE232F-006@student.nibm.lk>
2024-12-29 10:06:22 +0530

    add project

---
+++ src/test/java/com/example/eventflowsandbox/EventFlowSandBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventflowsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFlowSandBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
